12a273978bef739b545ed4e821bda3114acc8418
kimdlzp <kimdlzp@gmail.com>
2021-12-23 09:30:10 +0900

    5622

---
+++ BOJ/bronze1/1145.py
@@ -0,0 +1,13 @@
+a = list(map(int, input().split()))
+n = min(a)
+while True:
+    cnt = 0
+    for i in range(5):
+        if n % a[i] == 0:
+            cnt += 1
+        if cnt >2:
+            print(n)
+            break
+        n += 1
+
+        #브루트 포스 알고리즘
---
+++ BOJ/bronze2/5622.py
@@ -0,0 +1,11 @@
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ' ]
+
+a= input()
+ret = 0
+
+for j in range(len(a)):
+    for i in dial:
+        if a[j] in i:
+            ret += dial.index(i)+3
+
+print(ret)
