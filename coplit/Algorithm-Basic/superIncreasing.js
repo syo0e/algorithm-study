540a7cd964382370226add60421ff680f9f05177
kimdlzp <kimdlzp@gmail.com>
2022-02-15 12:57:02 +0900

    superIncreasing

---
+++ coplit/Algorithm-Basic/superIncreasing.js
@@ -0,0 +1,12 @@
+function superIncreasing(arr) {
+  let sum = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= sum) {
+      return false;
+    }
+    sum = sum + arr[i];
+  }
+  return true;
+}
+
+// 수를 요소로 갖는 배열을 입력받아 각 요소들이 그 이전의 요소들의 합보다 큰지 여부를 리턴해야 합니다.
