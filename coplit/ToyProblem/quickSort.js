9f98ce5853ca911d67bc9acaa720a21b5414fdaf
kimdlzp <kimdlzp@gmail.com>
2022-03-14 15:48:11 +0900

    quicksort

---
+++ coplit/ToyProblem/quickSort.js
@@ -0,0 +1,34 @@
+// naive solution
+// const quickSort = function (arr) {
+//   if (arr.length <= 1) return arr;
+
+//   const pivot = arr[0];
+//   const left = [];
+//   const right = [];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] <= pivot) left.push(arr[i]);
+//     else right.push(arr[i]);
+//   }
+
+//   const lSorted = quickSort(left);
+//   const rSorted = quickSort(right);
+//   return [...lSorted, pivot, ...rSorted];
+// };
+
+function quickSort(arr, transform = (item) => item) {
+  if (arr.length <= 1) return arr;
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (transform(arr[i]) < transform(pivot)) left.push(arr[i]);
+    else right.push(arr[i]);
+  }
+
+  const lSorted = quickSort(left, transform);
+  const rSorted = quickSort(right, transform);
+  return [...lSorted, pivot, ...rSorted];
+}
