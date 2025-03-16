48e07cbfc3889e5c50fe922e3daa9499a10c5d06
Syo0e <55309351+syo0e@users.noreply.github.com>
2025-03-16 23:36:09 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,7 +1,8 @@
 function solution(my_string, k) {
-    let answer = '';
-    for(i=1; i<=k; i++) {
-        answer += my_string;
-    }
-    return answer;
+    // let answer = '';
+    // for(i=1; i<=k; i++) {
+    //     answer += my_string;
+    // }
+    // return answer;
+    return my_string.repeat(k);
 }
